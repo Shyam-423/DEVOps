ab856e218db00fc0921c1a64f8b79b49c125c205
Shyam-423 <shyam2410533@ssn.edu.in>
2026-08-07 11:23:39 +0530

    Added Lab4 project

---
+++ Lab4/linux-lab/script.sh
@@ -0,0 +1,10 @@
+echo "Welcome to linux lab"
+echo "Current user:"
+ whoami
+echo "Current working directory:"
+ pwd
+echo  "Current data and time:"
+ date 
+echo "Course Name:"
+echo $COURSE_NAME
+
